d6e1570430c604f09e6d61c9b2e3c8730202146a
Pritamghara <pritamghara13@gmail.com>
2024-06-06 16:09:59 +0530

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,12 @@ const Header = () => {
     <header className='fixed top-0 left-0 w-full z-50 bg-black bg-opacity-50 backdrop-blur-lg'>
       <div className='p-2 md:px-20 flex justify-between items-center mx-auto'>
         <Link to='/'>
-        
-        <span className='text-4xl font-bold text-white'>CleverBooks</span>
+          <span className='text-2xl md:text-4xl font-bold text-white'>CleverBooks</span>
         </Link>
 
         {/* Central navigation with links */}
-        <nav className='text-white'>
-          <ul className='flex gap-8 text-xl'>
+        <nav className='hidden md:block text-white'>
+          <ul className='flex gap-8 text-lg'>
             <li className='hover:text-orange-300 transition duration-300'>Product</li>
             <li className='hover:text-orange-300 transition duration-300'>Pricing</li>
             <li className='hover:text-orange-300 transition duration-300'>Industry</li>
@@ -25,14 +24,14 @@ const Header = () => {
         {/* Login link and Talk to us button */}
         <div className='flex gap-6 items-center'>
           <Link to="/login">
-            <h2 className=' font-semibold text-xl bg-gradient-to-br from-red-300 to-purple-800 bg-clip-text text-transparent transition delay-800 hover:text-white'>
+            <h2 className='hidden md:block font-semibold text-lg bg-gradient-to-br from-red-300 to-purple-800 bg-clip-text text-transparent transition delay-800 hover:text-white'>
               Login
             </h2>
           </Link>
 
-          <button className='p-3 rounded-full overflow-hidden text-white  hover:text-black  font-semibold'>
-            <div className='w-32 h-16 flex items-center justify-center bg-gradient-to-br from-red-300 to-purple-800 hover:bg-gradient-to-tr hover:from-purple-800 hover:to-red-300 rounded-full'>
-              <span className=' text-xl'>
+          <button className='p-2 md:p-3 rounded-full overflow-hidden text-white font-semibold'>
+            <div className='px-4 py-3 hidden md:flex items-center justify-center bg-gradient-to-br from-red-300 to-purple-800 hover:bg-gradient-to-tr hover:from-purple-800 hover:to-red-300 rounded-full'>
+              <span className='text-lg'>
                 Talk to us
               </span>
             </div>
